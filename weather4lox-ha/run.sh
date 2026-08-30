35ff434e735e96c97e3962bb01ad808972ea4f9b
hagenbogner-prog <hagen.bogner@gmail.com>
2026-08-30 23:53:25 +0200

    feat: run Weather4Lox HA 0.3.5 compatibility layer

--- weather4lox-ha/run.sh
+++ weather4lox-ha/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-bashio::log.info "Starting Weather4Lox HA test service"
-exec python3 /server.py
+bashio::log.info "Starting Weather4Lox HA 0.3.5 service"
+exec python3 /server_035.py
