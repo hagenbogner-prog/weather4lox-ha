b1a179161bacb8aa9fd827c9cd3a4e9e6d8f47b2
hagenbogner-prog <hagen.bogner@gmail.com>
2026-08-31 22:04:36 +0200

    chore: bump add-on runtime version to 0.4.2

--- weather4lox-ha/run.sh
+++ weather4lox-ha/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-bashio::log.info "Starting Weather4Lox HA 0.4.1 service"
+bashio::log.info "Starting Weather4Lox HA 0.4.2 service"
 exec python3 /bootstrap.py
