59a9119b566f9a5db8181e6a0b1387171ab9c63b
hagenbogner-prog <hagen.bogner@gmail.com>
2026-08-31 21:17:23 +0200

    chore: align add-on runtime version

--- weather4lox-ha/run.sh
+++ weather4lox-ha/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-bashio::log.info "Starting Weather4Lox HA 0.3.9 service"
+bashio::log.info "Starting Weather4Lox HA 0.4.1 service"
 exec python3 /bootstrap.py
